186604ec7b6e7c481090ff6600317c8657c435ba
Rinon <rinon.ten@onja.org>
2022-03-11 10:25:55 +0300

    Fetch the repos and display it on the homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import useSWR from "swr";
+
+const RepositoriesList = (props: any) => {
+  return (
+    <div className="list_container">
+      <h2>{props.name}</h2>
+      <div className="description_container">
+        <p>
+          {props.description ? props.description : "No description provided!"}
+        </p>
+      </div>
+      <div className="owner_container">
+        <span>
+          Owner: <a href={props.owner.html_url}>{props.owner.login}</a>
+        </span>
+        <span>Repo's visibility: {props.visibility}</span>
+      </div>
+    </div>
+  );
+};
 
 function App() {
+  const fetcher = () =>
+    fetch("https://api.github.com/users/ganamavo/repos").then((res) =>
+      res.json()
+    );
+  const { data, error } = useSWR("/api/user/123", fetcher);
+
+  console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {!data && (
+        <div>
+          <p>Loading...</p>
+        </div>
+      )}
+      {error && (
+        <div>
+          <p>{error}</p>
+        </div>
+      )}
+      {data &&
+        data.map((repo: any) => <RepositoriesList key={repo.id} {...repo} />)}
     </div>
   );
 }
